51cf17faf78078c400f866ebfbe138d817f69ca9
Mateusz Krasuski <satoschi358@gmail.com>
2023-05-03 15:15:20 +0200

    Update project.

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,32 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+mod router;
+mod pages;
+mod components;
+
+use pages::{home::Home, notfound::NotFound, projects::Projects};
+use router::Route;
+use wasm_bindgen::prelude::*;
+use yew::{prelude::*, Renderer};
+use yew_router::{ BrowserRouter, Switch };
+
+fn switch( route: Route ) -> Html {
+    match route {
+        Route::Home => html!{ <Home /> },
+        Route::Projects => html!{ <Projects /> },
+        _ => html!{ <NotFound /> }
+    }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+#[function_component(Main)]
+pub fn app() -> Html {
+    html!{
+        <BrowserRouter>
+            <Switch<Route> render={switch} />
+        </BrowserRouter>
     }
+}
+
+
+#[wasm_bindgen(start)]
+fn run() {
+    Renderer::<Main>::new().render();
 }
